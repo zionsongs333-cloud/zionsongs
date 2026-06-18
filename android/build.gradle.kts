12e3b5df6afa2126d7807cfcfb5b07e940370fdd
Your Name <you@example.com>
2026-06-18 04:39:18 +0000

    gradle 8.10.2 no buildscript

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,3 @@
-buildscript {
-    dependencies {
-        classpath("org.codehaus.groovy:groovy-xml:3.0.19")
-        
 allprojects {
     repositories {
         google()
@@ -13,6 +9,7 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
